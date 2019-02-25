8dbe5abb72197f626b7c3f0393a87f6b758ab060
Andrew Linck <alinck@purdue.edu>
2019-02-25 17:49:39 -0500

    all

--- models/Product.js
+++ models/Product.js
@@ -1,10 +1,10 @@
 module.exports = function(connection, Sequelize) {
-    const Product = connection.define('Product', {
-      product_name: Sequelize.STRING,
-      department_name: Sequelize.INT,
-      price: Sequelize.INT,
-      stock_quantity: Sequelize.INT
-    });
+  const Product = connection.define('Product', {
+    product_name: Sequelize.STRING,
+    department_name: Sequelize.STRING,
+    price: Sequelize.INT,
+    stock_quantity: Sequelize.INT
+  });
 
-    return Product;
-  };
+  return Product;
+};
